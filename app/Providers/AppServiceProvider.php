ed312a9ba71cde84994a7c7fd0d0c10a56262cd2
KONICCO <nico678514@gmail.com>
2024-11-15 14:58:38 +0700

    finish blade tutorial

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Person;
+use App\Services\SayHello;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // injection
+        $this->app->singleton(SayHello::class, function(){
+            return new SayHello();
+        });
     }
 
     /**
@@ -23,6 +29,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // extending blade | membuat directive sendiri
+        Blade::directive("hello", function ($expression){
+            return "<?php echo 'Hello ' . $expression; ?>";
+        });
+        // custom echo handler
+        Blade::stringable(Person::class, function (Person $person){
+            return "$person->name : $person->address";
+        });
     }
 }
